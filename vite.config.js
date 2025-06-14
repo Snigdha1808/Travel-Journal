f3fbb57c30dbd25aa6b41c5f3e7db3da2b9229b1
Siripurapu Snigdha <141983332+Snigdha1808@users.noreply.github.com>
2025-06-14 15:13:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Travel-Journal/',
   plugins: [react()],
-  base: '/Travel-Journal/', 
 })
